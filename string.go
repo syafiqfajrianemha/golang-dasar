123866988ec8914fb55303013124fb313f6f63e6
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-03-01 05:55:27 +0700

    tipe data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Syafiq")
+	fmt.Println("Syafiq Fajrian")
+	fmt.Println("Syafiq Fajrian Emha")
+
+	fmt.Println(len("Syafiq"))
+	fmt.Println("Syafiq Fajrian"[0])
+	fmt.Println("Syafiq Fajrian Emha"[1])
+}
