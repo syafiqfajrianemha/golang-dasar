ff6d79db771438e0c456ea94fed58329c936e8ef
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-03-01 06:03:24 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Syafiq Fajrian Emha"
+	fmt.Println(name)
+
+	name = "Syafiq Emha"
+	fmt.Println(name)
+
+	var age = 21
+	fmt.Println(age)
+
+	gender := "Male"
+	fmt.Println(gender)
+
+	var (
+		firstName = "Syafiq Fajrian"
+		lastName  = "Emha"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
