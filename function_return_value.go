9c0fd835e04a4558b3b33b7170bc08126d852369
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-03-01 13:06:37 +0700

    function return value

---
+++ function_return_value.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func getHello(name string) string {
+	hello := "Hello " + name
+	return hello
+}
+
+func main() {
+	result := getHello("Syafiq")
+	fmt.Println(result)
+
+	fmt.Println(getHello("Budi"))
+}
